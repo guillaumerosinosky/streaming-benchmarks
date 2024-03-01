0a78f8540e5d2b303fdaee00b85cc60b37be3ebc
elkhan-shahverdi <shahverdiyev90@gmail.com>
2024-03-01 10:45:14 -0700

    Kafka Stream Works

--- setup.sh
+++ setup.sh
@@ -1,112 +1,129 @@
 #! /usr/bin/env bash
-
-
-./stream-bench.sh SETUP
 . ./variable.sh --source-only
 
-#FLINK SETUP
-sed -i 's/taskmanager.heap.mb: 1024/taskmanager.heap.mb: 6144/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
-sed -i 's/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: 4/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
-sed -i 's/jobmanager.rpc.address: localhost/jobmanager.rpc.address: stream-node-01/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
+function flink_setup() {
+    sed -i 's/taskmanager.heap.mb: 1024/taskmanager.heap.mb: 6144/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
+    sed -i 's/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: 4/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
+    sed -i 's/jobmanager.rpc.address: localhost/jobmanager.rpc.address: stream-node-01/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
 
-sed -i 's/taskmanager.heap.mb: 6144/taskmanager.heap.mb: 15360/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
-sed -i 's/taskmanager.numberOfTaskSlots: 4/taskmanager.numberOfTaskSlots: 8/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
+    sed -i 's/taskmanager.heap.mb: 6144/taskmanager.heap.mb: 15360/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
+    sed -i 's/taskmanager.numberOfTaskSlots: 4/taskmanager.numberOfTaskSlots: 8/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
 
-sed -i 's/taskmanager.heap.mb: 15360/taskmanager.heap.mb: 30720/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
-sed -i 's/taskmanager.numberOfTaskSlots: 8/taskmanager.numberOfTaskSlots: 16/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
-sed -i 's/jobmanager.heap.mb: 1024/jobmanager.heap.mb: 15360/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
+    sed -i 's/taskmanager.heap.mb: 15360/taskmanager.heap.mb: 30720/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
+    sed -i 's/taskmanager.numberOfTaskSlots: 8/taskmanager.numberOfTaskSlots: 16/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
+    sed -i 's/jobmanager.heap.mb: 1024/jobmanager.heap.mb: 15360/g' /root/stream-benchmarking/"${FLINK_DIR}"/conf/flink-conf.yaml
 
-cp /dev/null /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-# shellcheck disable=SC2129
-echo "stream-node-02" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-echo "stream-node-03" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-echo "stream-node-04" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-echo "stream-node-05" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-echo "stream-node-06" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-echo "stream-node-07" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-echo "stream-node-08" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-echo "stream-node-09" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-echo "stream-node-10" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
-
-
-cp /dev/null /root/stream-benchmarking/"${FLINK_DIR}"/conf/masters
-echo "stream-node-01" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/masters
-
-#SPARK SETUP
-cp /dev/null /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-# shellcheck disable=SC2129
-echo "stream-node-02" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-echo "stream-node-03" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-echo "stream-node-04" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-echo "stream-node-05" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-echo "stream-node-06" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-echo "stream-node-07" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-echo "stream-node-08" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-echo "stream-node-09" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
-echo "stream-node-10" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    cp /dev/null /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    # shellcheck disable=SC2129
+    echo "stream-node-02" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    echo "stream-node-03" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    echo "stream-node-04" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    echo "stream-node-05" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    echo "stream-node-06" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    echo "stream-node-07" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    echo "stream-node-08" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    echo "stream-node-09" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
+    echo "stream-node-10" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/workers
 
 
-cp /dev/null /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
-# shellcheck disable=SC2129
-echo "#!/usr/bin/env bash" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
-echo "SPARK_DRIVER_MEMORY=30G" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
-echo "SPARK_EXECUTOR_CORES=16" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
-echo "SPARK_EXECUTOR_MEMORY=30G" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
-echo "SPARK_WORKER_CORES=16" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
-echo "SPARK_WORKER_MEMORY=30g" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
-echo "SPARK_DAEMON_MEMORY=30g" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
-chmod +x /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    cp /dev/null /root/stream-benchmarking/"${FLINK_DIR}"/conf/masters
+    echo "stream-node-01" >> /root/stream-benchmarking/"${FLINK_DIR}"/conf/masters
+}
 
-##STORM SETUP
-#cp /dev/null /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-## shellcheck disable=SC2129
-#echo "storm.zookeeper.servers:" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#echo "    - \"zookeeper-node-01\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#echo "    - \"zookeeper-node-02\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#echo "    - \"zookeeper-node-03\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#echo "storm.zookeeper.port: 2181" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#echo "nimbus.childopts: \"-Xmx3g\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#echo "nimbus.seeds: [\"stream-node-01\"]" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#echo "supervisor.childopts: \"-Xmx1g -Djava.net.preferIPv4Stack=true]\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#echo "worker.childopts: \"-Xmx1g -Djava.net.preferIPv4Stack=true\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#
-
-#KAFKA SETUP
-sed -i 's/zookeeper.connect=localhost:2181/zookeeper.connect=zookeeper-node-01:2181,zookeeper-node-02:2181,zookeeper-node-03:2181/g' /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/server.properties
-
-sed -i 's/maxClientCnxns=0/maxClientCnxns=0/g' /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
-# shellcheck disable=SC2129
-echo "tickTime=2000" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
-echo "initLimit=20" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
-echo "syncLimit=10" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
-echo "server.1=zookeeper-node-01:2888:3888" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
-echo "server.2=zookeeper-node-02:2888:3888" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
-echo "server.3=zookeeper-node-03:2888:3888" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
-
-mkdir /tmp/zookeeper/ -p
-touch /tmp/zookeeper/myid
-echo '1' >> /tmp/zookeeper/myid
+function spark_setup() {
+    cp /dev/null /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    # shellcheck disable=SC2129
+    echo "stream-node-02" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    echo "stream-node-03" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    echo "stream-node-04" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    echo "stream-node-05" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    echo "stream-node-06" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    echo "stream-node-07" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    echo "stream-node-08" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    echo "stream-node-09" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
+    echo "stream-node-10" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/workers
 
 
+    cp /dev/null /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    # shellcheck disable=SC2129
+    echo "#!/usr/bin/env bash" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    echo "SPARK_DRIVER_MEMORY=30G" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    echo "SPARK_EXECUTOR_CORES=16" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    echo "SPARK_EXECUTOR_MEMORY=30G" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    echo "SPARK_WORKER_CORES=16" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    echo "SPARK_WORKER_MEMORY=30g" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    echo "SPARK_DAEMON_MEMORY=30g" >> /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
+    chmod +x /root/stream-benchmarking/"${SPARK_DIR}"/conf/spark-env.sh
 
-sed -i 's/maxClientCnxns=0/maxClientCnxns=0/g' /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
-# shellcheck disable=SC2129
-echo "tickTime=2000" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
-echo "initLimit=20" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
-echo "syncLimit=10" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
-echo "dataDir=/root/zookeeper" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
-echo "server.1=zookeeper-node-01:2888:3888" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
-echo "server.2=zookeeper-node-02:2888:3888" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
-echo "server.3=zookeeper-node-03:2888:3888" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+}
 
-mkdir /root/zookeeper/ -p
-touch /root/zookeeper/myid
-echo '1' >> /root/zookeeper/myid
+function storm_setup() {
+    cp /dev/null /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    # shellcheck disable=SC2129
+    echo "storm.zookeeper.servers:" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    echo "    - \"zookeeper-node-01\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    echo "    - \"zookeeper-node-02\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    echo "    - \"zookeeper-node-03\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    echo "storm.zookeeper.port: 2181" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    echo "nimbus.childopts: \"-Xmx3g\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    echo "nimbus.seeds: [\"stream-node-01\"]" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    echo "supervisor.childopts: \"-Xmx1g -Djava.net.preferIPv4Stack=true]\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+    echo "worker.childopts: \"-Xmx1g -Djava.net.preferIPv4Stack=true\"" >> /root/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
+}
 
-#JSTORM setup
+function kafka_setup() {
+    #KAFKA SETUP
+    sed -i 's/zookeeper.connect=localhost:2181/zookeeper.connect=zookeeper-node-01:2181,zookeeper-node-02:2181,zookeeper-node-03:2181/g' /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/server.properties
 
-#install 2.4.0 jar to maven repo
-mvn install:install-file -Dfile=/root/stream-benchmarking/jstorm-2.4.0/jstorm-core-2.4.0.jar -DgroupId=com.alibaba.jstorm -DartifactId=jstorm-core -Dversion=2.4.0 -Dpackaging=jar
+    sed -i 's/maxClientCnxns=0/maxClientCnxns=0/g' /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+    # shellcheck disable=SC2129
+    echo "tickTime=2000" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
+    echo "initLimit=20" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
+    echo "syncLimit=10" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
+    echo "server.1=zookeeper-node-01:2888:3888" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
+    echo "server.2=zookeeper-node-02:2888:3888" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
+    echo "server.3=zookeeper-node-03:2888:3888" >> /root/stream-benchmarking/"${KAFKA_DIR}"/config/zookeeper.properties
+
+    mkdir /tmp/zookeeper/ -p
+    touch /tmp/zookeeper/myid
+    echo "${HOSTNAME: -1}" >> /tmp/zookeeper/myid
+
+
+
+    sed -i 's/maxClientCnxns=0/maxClientCnxns=0/g' /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+    # shellcheck disable=SC2129
+    echo "tickTime=2000" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+    echo "initLimit=20" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+    echo "syncLimit=10" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+    echo "dataDir=/root/zookeeper" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+    echo "server.1=zookeeper-node-01:2888:3888" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+    echo "server.2=zookeeper-node-02:2888:3888" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+    echo "server.3=zookeeper-node-03:2888:3888" >> /root/stream-benchmarking/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
+
+    mkdir /root/zookeeper/ -p
+    touch /root/zookeeper/myid
+    echo '1' >> /root/zookeeper/myid
+}
+
+case $1 in
+    "flink")
+        flink_setup
+        ;;
+    "spark")
+        spark_setup
+        ;;
+    "storm")
+        storm_setup
+        ;;
+    "kafka")
+        kafka_setup
+        ;;
+    *)
+        echo "Invalid argument"
+        ;;
+esac
+
+
 
 ##Spark
 #./sbin/start-master.sh -h stream-node-01 -p 7077
@@ -151,7 +168,6 @@ mvn install:install-file -Dfile=/root/stream-benchmarking/jstorm-2.4.0/jstorm-co
 #scp "${STORM_DIR}"/conf/storm.yaml ubuntu@stream-node-08:~/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
 #scp "${STORM_DIR}"/conf/storm.yaml ubuntu@stream-node-09:~/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
 #scp "${STORM_DIR}"/conf/storm.yaml ubuntu@stream-node-10:~/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
-#scp "${STORM_DIR}"/conf/storm.yaml ubuntu@stream-node11:~/stream-benchmarking/"${STORM_DIR}"/conf/storm.yaml
 
 
 
