ad4063003d9eca8d9bfa7d9d0859e12c2623023a
elkhan-shahverdi <shahverdiyev90@gmail.com>
2024-03-03 16:39:51 -0700

    Kafka Stream Works

--- setup.sh
+++ setup.sh
@@ -98,6 +98,8 @@ function zookeeper_setup() {
     echo "server.1=zookeeper-node-01:2888:3888" >> /root/streaming-benchmarks/"${KAFKA_DIR}"/config/zookeeper.properties
     echo "server.2=zookeeper-node-02:2888:3888" >> /root/streaming-benchmarks/"${KAFKA_DIR}"/config/zookeeper.properties
     echo "server.3=zookeeper-node-03:2888:3888" >> /root/streaming-benchmarks/"${KAFKA_DIR}"/config/zookeeper.properties
+
+    sed -i "s/$(hostname)/0.0.0.0/g" /root/streaming-benchmarks/"${KAFKA_STREAM_DIR}"/config/zookeeper.properties
 }
 
 case $1 in
