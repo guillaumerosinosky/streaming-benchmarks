c8b8dc38d1600c390253d4790c0ddc2fa7008461
elkhan-shahverdi <shahverdiyev90@gmail.com>
2024-03-05 23:36:47 -0700

    R reporting fixed

--- spark-benchmarks/src/main/scala/spark/benchmark/AdvertisingSpark.scala
+++ spark-benchmarks/src/main/scala/spark/benchmark/AdvertisingSpark.scala
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Licensed under the terms of the Apache License 2.0. Please see LICENSE file in the project root for terms.
+ */
+
+// scalastyle:off println
+
 package spark.benchmark
 
 import benchmark.common.Utils
@@ -9,6 +16,7 @@ import redis.clients.jedis._
 import java.util
 import java.util.UUID
 import collection.JavaConverters._
+import scala.compat.Platform.currentTime
 
 object AdvertisingSpark {
 
@@ -29,18 +37,19 @@ object AdvertisingSpark {
     }
   }
 
-  private case class AdsFiltered(ad_id: String, event_time: String)
+  case class AdsFiltered(ad_id: String, event_time: String)
 
-  private case class AdsEnriched(campaign_id: String, ad_id: String, event_time: String)
+  case class AdsEnriched(campaign_id: String, ad_id: String, event_time: String)
 
   private case class AdsCalculated(ad_id: String, campaign_id: String, window_time: Long)
 
-  private case class AdsCounted(campaign_id: String, window_time: Long, count: Long = 0)
+  case class AdsCounted(campaign_id: String, window_time: Long, count: Long = 0)
 
 
   def main(args: Array[String]): Unit = {
-//    val commonConfig = Utils.findAndReadConfigFile("./conf/localConf.yaml", true).asInstanceOf[java.util.Map[String, Any]]
-    val commonConfig = Utils.findAndReadConfigFile(args(0), true).asInstanceOf[java.util.Map[String, Any]]
+
+    //        val commonConfig = Utils.findAndReadConfigFile("./conf/localConf.yaml", true).asInstanceOf[java.util.Map[String, Any]];
+    val commonConfig = Utils.findAndReadConfigFile(args(0), true).asInstanceOf[java.util.Map[String, Any]];
     val timeDivisor = commonConfig.get("time.divisor") match {
       case n: Number => n.longValue()
       case other => throw new ClassCastException(other + " not a Number")
@@ -118,6 +127,8 @@ object AdvertisingSpark {
     //project the event, basically filter the fields.
     val projected = filteredOnView.select("ad_id", "event_time").map(row => AdsFiltered(row.getAs(0), row.getAs(1)))
 
+
+    //Note that the Storm benchmark caches the results from Redis, we don't do that here yet
     val redisJoined = projected.mapPartitions(queryRedisTopLevel(_, redisHost))
 
 
@@ -134,15 +145,15 @@ object AdvertisingSpark {
 
     val writer = new ForeachWriter[((String, Long), Long)] {
 
-      override def open(partitionId: Long, version: Long): Boolean = {
+      override def open(partitionId: Long, version: Long) = {
         true
       }
 
-      override def process(value: ((String, Long), Long)): Unit = {
+      override def process(value: ((String, Long), Long)) = {
         writeRedisTopLevel(AdsCounted(value._1._1, value._1._2, value._2), redisHost)
       }
 
-      override def close(errorOrNull: Throwable): Unit = {
+      override def close(errorOrNull: Throwable) = {
       }
     }
     val writeToConsole = totalEventsPerCampaignTime
@@ -153,29 +164,29 @@ object AdvertisingSpark {
     spark.streams.awaitAnyTermination()
   }
 
-  private def joinHosts(hosts: Seq[String], port: String): String = {
-    val joined = new StringBuilder("")
+  def joinHosts(hosts: Seq[String], port: String): String = {
+    val joined = new StringBuilder("");
     hosts.foreach({
-      joined.append(",").append(_).append(":").append(port)
+      joined.append(",").append(_).append(":").append(port);
     })
-    joined.toString().substring(1)
-  }
-  //noinspection DuplicatedCode
-  private def queryRedisTopLevel(eventsIterator: Iterator[AdsFiltered], redisHost: String): Iterator[AdsEnriched] = {
-    val pool = new Pool(new JedisPool(new JedisPoolConfig(), redisHost, 6379, 2000))
-    val ad_to_campaign = new util.HashMap[String, String]()
-    val eventsIteratorMap = eventsIterator.map(event => queryRedis(pool, ad_to_campaign, event))
-    pool.underlying.getResource.close()
-    eventsIteratorMap
+    return joined.toString().substring(1);
   }
 
-  private def queryRedis(pool: Pool, ad_to_campaign: util.HashMap[String, String], event: AdsFiltered): AdsEnriched = {
+  def queryRedisTopLevel(eventsIterator: Iterator[AdsFiltered], redisHost: String): Iterator[AdsEnriched] = {
+    val pool = new Pool(new JedisPool(new JedisPoolConfig(), redisHost, 6379, 2000))
+    var ad_to_campaign = new util.HashMap[String, String]();
+    val eventsIteratorMap = eventsIterator.map(event => queryRedis(pool, ad_to_campaign, event))
+    pool.underlying.getResource.close()
+    return eventsIteratorMap
+  }
+
+  def queryRedis(pool: Pool, ad_to_campaign: util.HashMap[String, String], event: AdsFiltered): AdsEnriched = {
     val ad_id = event.ad_id
     val campaign_id_cache = ad_to_campaign.get(ad_id)
     if (campaign_id_cache == null) {
       pool.withJedisClient { client =>
         val campaign_id_temp = Dress.up(client).get(ad_id)
-        if (campaign_id_temp.isDefined) {
+        if (campaign_id_temp != None) {
           val campaign_id = campaign_id_temp.get
           ad_to_campaign.put(ad_id, campaign_id)
           AdsEnriched(campaign_id, event.ad_id, event.event_time)
@@ -189,8 +200,7 @@ object AdvertisingSpark {
     }
   }
 
-  //noinspection DuplicatedCode
-  private def writeRedisTopLevel(campaign_window_counts: AdsCounted, redisHost: String): Unit = {
+  def writeRedisTopLevel(campaign_window_counts: AdsCounted, redisHost: String) {
 
     val pool = new Pool(new JedisPool(new JedisPoolConfig(), redisHost, 6379, 2000))
     writeWindow(pool, campaign_window_counts)
@@ -218,7 +228,7 @@ object AdvertisingSpark {
         dressUp.lpush(windowListUUID, window_timestamp)
       }
       dressUp.hincrBy(windowUUID, "seen_count", window_seenCount)
-      dressUp.hset(windowUUID, "time_updated", System.currentTimeMillis().toString)
+      dressUp.hset(windowUUID, "time_updated", currentTime.toString)
       return window_seenCount.toString
     }
 
--- spark-benchmarks/src/main/scala/spark/benchmark/sedis.scala
+++ spark-benchmarks/src/main/scala/spark/benchmark/sedis.scala
@@ -1,73 +1,27 @@
 package spark.benchmark
 
-import scala.concurrent.duration._
-
 import redis.clients.jedis._
 
-trait Dress {
-  implicit def delegateToJedis(d: Wrap) = d.j
+import scala.language.implicitConversions
 
-  implicit def fromJedistoScala(j: Jedis) = up(j)
+trait Dress {
+  implicit def delegateToJedis(d: Wrap): Jedis = d.j
+
+  implicit def fromJedistoScala(j: Jedis): Wrap = up(j)
 
   class Wrap(val j: Jedis) {
     import collection.JavaConverters._
-
-    def expire(key: String, time: FiniteDuration) = {
-      j.expire(key, time.toSeconds.toInt)
-    }
-
-    def hmset(key: String, values: Map[String, String]) = {
-      j.hmset(key,values.asJava)
-    }
 
     def hmget(key: String, values: String*): List[Option[String]] = {
       j.hmget(key,values: _*).asScala.toList.map(Option.apply)
     }
 
-    def hget(key: String, field: String): Option[String] = {
-      Option(j.hget(key, field))
-    }
-
-    def hgetAll(key: String): Map[String,String] = {
-      j.hgetAll(key).asScala.toMap
-    }
-
-    def smembers(key: String):Set[String] = {
-      j.smembers(key).asScala.toSet
-    }
-
-    def sinter(key: String): Set[String] = {
-      j.sinter(key).asScala.toSet
-    }
-
-    def sunion(key: String): Set[String] = {
-      j.sunion(key).asScala.toSet
-    }
-
-    def sdiff(key: String): Set[String] = {
-      j.sdiff(key).asScala.toSet
-    }
-
-    def zrange(key: String, start: Long, end: Long): Set[String] = {
-      j.zrange(key, start, end).asScala.toSet
-    }
-
-
-    def hkeys(key: String): Set[String] = {
-      j.hkeys(key).asScala.toSet
-    }
-
-    def hvals(key: String): List[String] = {
-      j.hvals(key).asScala.toList
-    }
 
     def get(k: String): Option[String] = {
       val f = j.get(k)
-      if (f == null) None else Some(f)
+      Option(f)
     }
-    def lrange(key: String, start: Long, end: Long): List[String] = {
-      j.lrange(key,start,end).asScala.toList
-    }
+
     def sort(key: String, params: SortingParams): List[String] = {
       j.sort(key,params).asScala.toList
     }
@@ -75,21 +29,6 @@ trait Dress {
       j.sort(key).asScala.toList
     }
 
-    def blpop(timeout: Int, args: String*): List[String] = {
-      j.blpop(timeout, args:_*).asScala.toList
-    }
-
-    def blpop(args: String*): List[String] = {
-      j.blpop(args:_*).asScala.toList
-    }
-
-    def brpop(timeout: Int, args: String*): List[String] = {
-      j.brpop(timeout, args:_*).asScala.toList
-    }
-
-    def brpop(args: String*): List[String] = {
-      j.brpop(args:_*).asScala.toList
-    }
   }
   def up(j: Jedis) = new Wrap(j)
 }
@@ -99,14 +38,6 @@ object Dress extends Dress
 
 class Pool(val underlying: JedisPool) {
 
-  def withClient[T](body: Dress.Wrap => T): T = {
-    val jedis: Jedis = underlying.getResource
-    try {
-      body(Dress.up(jedis))
-    } finally {
-      underlying.returnResourceObject(jedis)
-    }
-  }
   def withJedisClient[T](body: Jedis => T): T = {
     val jedis: Jedis = underlying.getResource
     try {
@@ -115,26 +46,6 @@ class Pool(val underlying: JedisPool) {
       underlying.returnResourceObject(jedis)
     }
   }
-
 }
 
-class SentinelPool(val underlying: JedisSentinelPool) {
 
-  def withClient[T](body: Dress.Wrap => T): T = {
-    val jedis: Jedis = underlying.getResource
-    try {
-      body(Dress.up(jedis))
-    } finally {
-      underlying.returnResourceObject(jedis)
-    }
-  }
-  def withJedisClient[T](body: Jedis => T): T = {
-    val jedis: Jedis = underlying.getResource
-    try {
-      body(jedis)
-    } finally {
-      underlying.returnResourceObject(jedis)
-    }
-  }
-
-}
