2677a81ffbe37e52b411434ed553960f10d6c622
elkhan-shahverdi <shahverdiyev90@gmail.com>
2024-03-07 21:30:27 -0700

    R reporting fixed

--- stream-bench.sh
+++ stream-bench.sh
@@ -201,7 +201,7 @@ run() {
   elif [ "START_LOAD" = "$OPERATION" ];
   then
     cd data
-    start_if_needed leiningen.core.main "Load Generation" 1 $LEIN run -r -t $1 --configPath ../$CONF_FILE
+    start_if_needed leiningen.core.main "Load Generation" 1 $LEIN run -r -t $2 --configPath ../$CONF_FILE
     cd ..
   elif [ "STOP_LOAD" = "$OPERATION" ];
   then
@@ -254,7 +254,7 @@ run() {
     run "START_KAFKA"
     run "START_FLINK"
     run "START_FLINK_PROCESSING"
-    run "START_LOAD" $1
+    run "START_LOAD" $2
     sleep ${TEST_TIME}
     run "STOP_LOAD"
     run "STOP_FLINK_PROCESSING"
@@ -269,7 +269,7 @@ run() {
     run "START_KAFKA"
     run "START_JET"
     run "START_JET_PROCESSING"
-    run "START_LOAD" $1
+    run "START_LOAD" $2
     sleep ${TEST_TIME}
     run "STOP_LOAD"
     run "STOP_JET"
@@ -283,7 +283,7 @@ run() {
     run "START_KAFKA"
     run "START_SPARK"
     run "START_SPARK_PROCESSING"
-    run "START_LOAD" $1
+    run "START_LOAD" $2
     sleep ${TEST_TIME}
     run "STOP_LOAD"
     run "STOP_SPARK_PROCESSING"
@@ -297,7 +297,7 @@ run() {
     run "START_REDIS"
     run "START_KAFKA"
     run "START_KAFKA_PROCESSING"
-    run "START_LOAD" $1
+    run "START_LOAD" $2
     sleep ${TEST_TIME}
     run "STOP_LOAD"
     run "STOP_KAFKA_PROCESSING"
