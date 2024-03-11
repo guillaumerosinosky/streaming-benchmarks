01fd9d0a871675fc361edfdc570093a2a219ba17
elkhan-shahverdi <shahverdiyev90@gmail.com>
2024-03-11 15:29:01 -0600

    Kafka Half completed

--- remote.sh
+++ remote.sh
@@ -341,7 +341,7 @@ function stopAll (){
 }
 
 function benchmarkLoop (){
-  for i in $(seq 6 $BENCHMARK_COUNT); do
+  for i in $(seq 1 $BENCHMARK_COUNT); do
       CURRENT_TPS=$((i * INITIAL_TPS))
       echo "Benchmark $CURRENT_TPS"
       runAllServers "${PULL_GIT}"
